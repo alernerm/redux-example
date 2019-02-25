d76487df1921edf52ec33e426cf391f42ab33b7a
Alex <alernerm@gmail.com>
2019-02-25 13:34:44 -0500

    actions, reducers,etc

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends Component {
   constructor(props) {
@@ -23,15 +26,8 @@ class PostForm extends Component {
       body: this.state.body
     };
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(response => response.json())
-      .then(data => console.log(data));
+    //call action
+    this.props.createPost(post);
   }
 
   render() {
@@ -62,4 +58,11 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createPost }
+)(PostForm);
